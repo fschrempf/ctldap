434051e26c9f6719b5c86fe5434307232fcf2e67
Frieder Schrempf <frieder@fris.de>
2023-10-20 20:52:37 +0200

    ctservice.test.js: Fix getGroupMemberships test title

--- test/ctservice.test.js
+++ test/ctservice.test.js
@@ -24,7 +24,7 @@ describe('Church Tools Services', () => {
     const actual = await ctservice.getPersonsInGroups([], {});
     expect(actual).to.be.containingAllOf([1, 3, 2]);
   });
-  it('getPersonsInGroups - gets person group relations', async () => {
+  it('getGroupMemberships - gets person group relations', async () => {
     const expected = [
       { personId: 1, groupId: 1 },
       { personId: 3, groupId: 2 },
